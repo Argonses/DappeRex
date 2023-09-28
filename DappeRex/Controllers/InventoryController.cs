a1804dcc70dbf443fa1c7ed6181f218abc9f5efd
resr <vaclavsr@post.cz>
2023-09-28 08:26:45 +0200

    Added model, controller and first API

---
+++ DappeRex/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using DappeRex.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DappeRex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public InventoryController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> GetAllItems()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var items = await connection.QueryAsync<Item>("select * from DappeRex");
+            return Ok(items);
+        }
+    }
+}
---
+++ DappeRex/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace DappeRex.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public float Price { get; set; } 
+        public int StockQuantity { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public DateTime AddedDate { get; set; } = DateTime.UtcNow;
+    }
+}
